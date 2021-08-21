3fab6ee3231824d7789706b066984d4c16f1da7e
Mansimar Anand <anandmansimar@gmail.com>
2021-08-21 17:27:30 +0530

    Create source file

---
+++ source.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <chrono>
+
+#include "./header_files/bubble_sort.h"
+#include "./header_files/insertion_sort.h"
+
+using namespace std;
+
+template <typename T>
+inline void print(T* arr, int size) {
+	for(int index = 0 ; index < size ; ++index) {
+		cout << arr[index] << ' ';
+	}
+	cout << '\n';
+}
+
+int main()
+{
+	int size;
+	cin >> size;
+
+	int* arr = new int[size];
+	for(int i = 0 ; i < size ; ++i) {
+		cin >> arr[i];
+	}
+
+	enum algo_code {
+		_bubble_sort,
+		_insertion_sort,
+		_merge_sort,
+		_quick_sort,
+		_twist_sort,
+	};
+
+	string algos[] = {"bubble_sort", "insertion_sort", "merge_sort", "quick_sort", "twist_sort"};
+
+	for(int curr = 0 ; curr < 5 ; ++curr) {
+
+		auto start = chrono::high_resolution_clock::now();
+
+		switch(curr) {
+			case _bubble_sort:
+				print<int>(bubble_sort<int>(arr, size), size);
+				break;
+
+			case _insertion_sort:
+				print<int>(insertion_sort<int>(arr, size), size);
+				break;
+
+			case _merge_sort:
+				print<int>(bubble_sort<int>(arr, size), size);
+				break;
+
+			case _quick_sort:
+				print<int>(bubble_sort<int>(arr, size), size);
+				break;
+
+			case _twist_sort:
+				print<int>(bubble_sort<int>(arr, size), size);
+				break;
+
+			default:
+				break;
+		}
+
+		auto stop = chrono::high_resolution_clock::now();
+		auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+
+		cout << algos[curr] << " took: " << duration.count() << " microseconds" << '\n';
+	}
+
+	return 0;
+}
